9e29793ff79fc95b2ca057542ae482daa04df911
Arbeen <arbinbabuthapamagar2002@gmail.com>
2026-05-12 11:58:17 +0545

    Enhance product loader and UI: update GraphQL query, format product data, and implement product listing with pagination

--- app/routes/app.product.jsx
+++ app/routes/app.product.jsx
@@ -30,7 +30,14 @@ export const loader = asyncHandler(async ({ request }) => {
     edges {
     node{
     id 
-    title 
+    title
+    status
+    images(first:1){
+    edges{
+    node{
+    url
+    altText}}
+    }
 }} pageInfo{
  hasNextPage
  endCursor }
@@ -47,9 +54,56 @@ export const loader = asyncHandler(async ({ request }) => {
     throw new apiError(404, " Product data not found ");
   }
   //  making it into format
-  const products = result.data.products.edges.map((e) => e.node);
+  const products = result.data.products.edges.map((e) => ({
+    ...e.node,
+       image: e.node.images.edges[0]?.node?.url || "" }));
+
   const pageInfo = result.data.products.pageInfo;
   console.log(products);
 
   return data({ products, pageInfo });
 });
+
+// frontend
+
+// making ui
+
+export default function () {
+  const { products,pageInfo } = useLoaderData();
+  console.log(products);
+
+  const navigate = useNavigate();
+
+  const list = products.map((p) => [
+    p.id,
+    p.title,
+    p.status,
+    p.image
+  ]);
+
+  return (
+  
+   <Page
+      title="Products"
+      primaryAction={{ content: "Edit", onAction: () => {} }}
+    >
+      <Card>
+        <DataTable
+          columnContentTypes={["text", "text", "text", "text"]}
+          headings={["Image", "Title", "Status", "Action"]}
+          rows={products.map((p) => [
+            <Thumbnail source={p.image} alt={p.title} size="small" />,
+            p.title,
+            <Badge>{p.status}</Badge>,
+            <Button onClick={()=>navigate('/app/product/hj')} variant="plain">View</Button>,
+          ])}
+        />
+      </Card>
+      <Pagination
+        hasNext={pageInfo.hasNextPage}
+        onNext={() => navigate(`?cursor=${pageInfo.endCursor}`)}
+      />
+    </Page>
+  
+)
+}
